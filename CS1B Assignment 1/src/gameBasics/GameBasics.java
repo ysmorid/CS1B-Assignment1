cb24cd4cc9aba61a4f0110b71dd42018d2bae68b
Ylia Morid <ylsmo15@gmail.com>
2016-10-11 22:30:21 -0700

    Create and print new Card objects.
    Modify and re-print card objects in GameBasics class.

--- CS1B Assignment 1/src/gameBasics/GameBasics.java
+++ CS1B Assignment 1/src/gameBasics/GameBasics.java
@@ -3,7 +3,22 @@ package gameBasics;
 public class GameBasics {
 
    public static void main(String[] args) {
-      // TODO Auto-generated method stub
+
+      Card card1 = new Card();
+      Card card2 = new Card('p', Card.Suit.DIAMONDS);
+      Card card3 = new Card('j', Card.Suit.CLUBS);
+      
+      System.out.println(card1.toString());
+      System.out.println(card2.toString());
+      System.out.println(card3.toString());
+      System.out.println("");
+      
+      card1.setCard('1', Card.Suit.HEARTS);
+      card2.setCard('Q', Card.Suit.SPADES);
+      
+      System.out.println(card1.toString());
+      System.out.println(card2.toString());
+      System.out.println(card3.toString());
 
    }
 
