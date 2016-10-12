a4bc4bca84568013591922106950db86f8aad181
Ylia Morid <ylsmo15@gmail.com>
2016-10-11 23:20:59 -0700

    Hand: Create default constructor
    resetHand() method that sets array myCards back to null
    
    HandTest:
    Set up Junit test class for Hand class

--- CS1B Assignment 1/src/gameBasics/Hand.java
+++ CS1B Assignment 1/src/gameBasics/Hand.java
@@ -2,18 +2,15 @@ package gameBasics;
 
 public class Hand {
 
+   public final int MAX_CARDS = 100;
+   private Card[] myCards;
    private int numCards;
    
    public Hand(){
-      
    }
    
-   public void takeCard(){
-      
-   }
-   
-   public void playCard(){
-      
+   public void resetHand(){
+      myCards = new Card[0];
    }
    
 }
---
+++ CS1B Assignment 1/src/testPackage/HandTest.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import gameBasics.Card;
+import gameBasics.Hand;
+
+public class HandTest {
+   Hand test = new Hand();
+
+
+}
